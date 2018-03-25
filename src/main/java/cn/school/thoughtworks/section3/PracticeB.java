80dde9465fe3bec7769340a00a78aa3c58941828
Lucerne <lucerne_liu@foxmail.com>
2018-03-25 23:15:13 +0800

    finish section3 PracticeB

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -1,12 +1,18 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        //选出collection1中key跟collection2中value对应的list中的元素相同的元素,把他们的count，满3减1，输出减过之后的新集合
+        ArrayList<String> list2 = new ArrayList<>(object.get("value"));
+        int count = 0;
+        for (String str : list2) {
+            count = collectionA.get(str);
+            collectionA.put(str, count - count/3);
+        }
+        return collectionA;
     }
 }
