5057791e6e6668c55f5d3184b832adc5b618bf97
Lucerne <lucerne_liu@foxmail.com>
2018-03-25 23:02:31 +0800

    finish section2 PracticeC

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -7,14 +7,16 @@ public class PracticeB {
         //把1集合中相同的元素统计出数量
         ArrayList<String> list1 = new ArrayList<>(collection1);
         Map<String, Integer> result = new HashMap<>();
-        int temp = 1;
+        int temp;
         for (String list : list1) {
             if (list.contains("-")) {
                 temp = Integer.parseInt(list.split("-")[1]);
                 list = list.split("-")[0];
+            }else{
+                temp = 1;
             }
             if (result.containsKey(list)) {
-                result.put(list, result.get(list) + 1);
+                result.put(list, result.get(list) + temp);
             } else {
                 result.put(list, temp);
             }
--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,34 @@
 package cn.school.thoughtworks.section2;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        //把1集合中相同的元素统计出数量
+        ArrayList<String> list1 = new ArrayList<>(collection1);
+        Map<String, Integer> result = new HashMap<>();
+        int temp = 1;
+        String str;
+        for (String list : list1) {
+            str = list;
+            temp = 1;
+            String concatNum = "";
+            for (int i = 0; i < list.length(); i++) {
+                if (list.charAt(i) >= 48 && list.charAt(i) <= 57) {
+                    concatNum += (list.charAt(i) - 48)+"";
+                }
+            }
+            if (concatNum != ""){
+                temp = Integer.parseInt(concatNum);
+                str = String.valueOf(list.charAt(0));
+            }
+            if (result.containsKey(str)) {
+                result.put(str, result.get(str) + temp);
+            } else {
+                result.put(str, temp);
+            }
+        }
+        return result;
     }
+
 }
